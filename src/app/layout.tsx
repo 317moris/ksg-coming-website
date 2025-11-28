ec93cff371306c44a9cd0e43ff1e0acd42ede277
317moris <mori.souta.fu@st.spec.ed.jp>
2025-11-28 21:57:39 +0900

    interだけ追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import { Provider } from "@/components/ui/provider";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+	variable: "--font-inter",
+	subsets: ["latin"],
+});
 
 export default function RootLayout({
 	children,
@@ -6,7 +12,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="ja" suppressHydrationWarning>
+		<html lang="ja" className={inter.variable} suppressHydrationWarning>
 			<body>
 				<Provider>{children}</Provider>
 			</body>
